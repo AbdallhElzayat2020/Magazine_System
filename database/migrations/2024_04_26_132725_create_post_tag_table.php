0a768955237534679218f079f62139166961477d
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-04-27 06:05:16 +0300

    end DB

--- database/migrations/2024_04_26_132725_create_post_tag_table.php
+++ database/migrations/2024_04_26_132725_create_post_tag_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -12,11 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            // $x = 2;
-            // $y = 2;
-            // echo $x;
-            // echo '<br>';
-            // echo $y;
             $table->id();
             $table->integer('post_id');
             $table->integer('tag_id');
